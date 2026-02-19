a2e98e4805e1c8ccec200a6473fa6004808ea3fa
kandala karthik <kandalakarthik3@gmail.com>
2026-02-19 16:19:12 +0530

    Initial clean commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import Classclick from './components/Classclick';
-import Eventbind from './components/Eventbind';
-import Prent from './components/Prent';
-import Nmaelist from './components/Nmaelist';
-import Stylesheet from './components/Stylesheet';
-import Inline from './components/Inline';
-// import Form from './components/Form';
-import Post from './httpcompo/Post';
-import New from './Func/New';
-import Onchange from './Func/Onchange1';
-// import Form from './components/Form';
-import Form from './Func/Form';
+// import Greet from './components/Greet';
+// import Welcome from './components/Welcome';
+// import Hello from './components/Hello';
+// import Message from './components/Message';
+// import Counter from './components/Counter';
+// import FunctionClick from './components/FunctionClick';
+// import Classclick from './components/Classclick';
+// import Eventbind from './components/Eventbind';
+// import Prent from './components/Prent';
+// import Nmaelist from './components/Nmaelist';
+// import Stylesheet from './components/Stylesheet';
+// import Inline from './components/Inline';
+// // import Form from './components/Form';
+// import Post from './httpcompo/Post';
+// import New from './Func/New';
+// import Onchange from './Func/Onchange1';
+// // import Form from './components/Form';
+// import Form from './Func/Form';
 import Todo from './Func/Todo';
 
 
